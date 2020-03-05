2d1051521b24ff4dcfbcbee9e9ad47b67e2ca486
toyowata <toyomasa.watarai@gmail.com>
2020-03-05 22:50:08 +0900

    Add code

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "platform/mbed_thread.h"
+#include "USBSerial.h"
+
+USBSerial usbs;
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE_MS                                                    500
+
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+    usbs.printf("hello, USBDevice\n");
+
+    while (true) {
+        led = !led;
+        thread_sleep_for(BLINKING_RATE_MS);
+    }
+}
